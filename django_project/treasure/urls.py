66e795a8a18ba9156b2994554e74c9f4a125fab9
hoodscott <scottdothood@gmail.com>
2015-11-16 15:34:05 +0000

    Adding login/logout and restricting access to some pages

--- django_project/treasure/urls.py
+++ django_project/treasure/urls.py
@@ -3,15 +3,16 @@ from treasure import views
 
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
-		url(r'^about/', views.about, name='about'),
+	url(r'^about/', views.about, name='about'),
         url(r'^search/$', views.search, name='search'),
         url(r'^resource/(?P<mresource_id>\w+)/$', views.resource, name='resource'),
         url(r'^add_resource/$', views.add_resource, name='add_resource'),
-        url(r'^add_teacher/$', views.add_teacher, name='add_teacher'),
         url(r'^add_hub/$', views.add_hub, name='add_hub'),
         url(r'^add_school/$', views.add_school, name='add_school'),
         url(r'^profile/$', views.profile, name='profile'),
         url(r'^profile/(?P<user_id>\w+)/$', views.profile, name='profile'),
         url(r'^profile/(?P<user_id>\w+)/history/$', views.user_history, name='user_history'),
-        url(r'^register/$', views.register, name='register')
+        url(r'^register/$', views.register, name='register'),
+        url(r'^login/$', views.user_login, name='login'),
+        url(r'^logout/$', views.user_logout, name='logout'),
         )
--- django_project/treasure/views.py
+++ django_project/treasure/views.py
@@ -3,6 +3,8 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response, redirect
 from treasure.forms import *
 from treasure.models import *
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # view for the homepage
 def index(request):
@@ -56,7 +58,6 @@ def resource(request):
 def add_resource(request):
     # get context of request
     context = RequestContext(request)
-
     
     # A HTTP POST?
     if request.method == 'POST':
@@ -80,44 +81,9 @@ def add_resource(request):
     # create dictionary to pass data to templates
     context_dict = {'form': form}
     
-
-
-    
     # Bad form (or form details), no form supplied...
     # Render the form with error messages (if any).
     return render_to_response('treasure/add_resource.html', context_dict, context)
-
-# view for the add teacher page
-def add_teacher(request):
-    # get context of request
-    context = RequestContext(request)
-
-    
-    # A HTTP POST?
-    if request.method == 'POST':
-        form = TeacherForm(request.POST)
-
-        # Have we been provided with a valid form?
-        if form.is_valid():
-            # Save the new category to the database.
-            form.save(commit=True)     
-            
-            # Now call the index() view.
-            # The user will be shown the homepage.
-            return index(request)
-        else:
-            # The supplied form contained errors - just print them to the terminal.
-            print form.errors
-    else:
-        # If the request was not a POST, display the form to enter details.
-        form = TeacherForm()
-    
-    # create dictionary to pass data to templates
-    context_dict = {'form': form}
-    
-    # Bad form (or form details), no form supplied...
-    # Render the form with error messages (if any).
-    return render_to_response('treasure/add_teacher.html', context_dict, context)
     
 # view for the user's profile page
 def profile(request):
@@ -143,6 +109,7 @@ def user_history(request):
     return render_to_response('treasure/user_history.html', context_dict, context)
     
 # view for the add hub page
+@login_required
 def add_hub(request):
     # get context of request
     context = RequestContext(request)
@@ -175,6 +142,7 @@ def add_hub(request):
     return render_to_response('treasure/add_hub.html', context_dict, context)
     
 # view for the add users page
+@login_required
 def add_school(request):
     # get context of request
     context = RequestContext(request)
@@ -262,3 +230,50 @@ def register(request):
             'treasure/register.html',
             {'user_form': user_form, 'teacher_form': teacher_form, 'registered': registered},
             context)
+            
+         
+def user_login(request):
+    # Like before, obtain the context for the user's request.
+    context = RequestContext(request)
+
+    # If the request is a HTTP POST, try to pull out the relevant information.
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Use Django's machinery to attempt to see if the username/password
+        # combination is valid - a User object is returned if it is.
+        user = authenticate(username=username, password=password)
+
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value), no user
+        # with matching credentials was found.
+        if user:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+                login(request, user)
+                return HttpResponseRedirect('/treasure/')
+            else:
+                # An inactive account was used - no logging in!
+                return HttpResponse("Your account is disabled.")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+
+    # The request is not a HTTP POST, so display the login form.
+    # This scenario would most likely be a HTTP GET.
+    else:
+        return render_to_response('treasure/login.html', {}, context)
+
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+
+    # Take the user back to the homepage.
+    return HttpResponseRedirect('/treasure/')
