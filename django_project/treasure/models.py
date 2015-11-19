d9e09b15e0c55a64161766b615d86a181610ef39
hoodscott <scottdothood@gmail.com>
2015-11-19 21:33:24 +0000

    Updating models with latest database design #1

--- django_project/treasure/models.py
+++ django_project/treasure/models.py
@@ -16,95 +16,188 @@ class Teacher(models.Model):
         
 # model for schools
 class School(models.Model):
-    schoolname = models.CharField(max_length=128)
-    location = models.CharField(max_length=128) #lat long?
-    address = models.CharField(max_length=128)
+    name = models.CharField(max_length=128)
+    town = models.CharField(max_length=128)
+    address = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    # creates a many to many relationship with teachers
+    teachers = models.ManyToManyField(Teacher)
 
     def __unicode__(self):
-        return self.schoolname
-
-# relationship between teachers and schools
-class TeacherSchool(models.Model):
-	# //todo foreignkeys
-    #teacher = models.ForeignKey(Teacher)
-    #school = models.ForeignKey(School)
-
-    def __unicode__(self):
-        return self.teacher.user.username, self.school.schoolname        
+        return self.name       
 
 # model for hubs        
 class Hub(models.Model):
-    hubname = models.CharField(max_length=128)
-    location = models.CharField(max_length=128) #lat long?
-    address = models.CharField(max_length=128)
+    name = models.CharField(max_length=128)
+    address = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    # creates a many to many relationship with teachers
+    teachers = models.ManyToManyField(Teacher)
 
     def __unicode__(self):
-        return self.hubname
-        
-# relationship between teachers and hubs
-class TeacherHub(models.Model):
-	# //todo foreignkeys
-    #teacher = models.ForeignKey(Teacher)
-    #hub = models.ForeignKey(Hub)
-
-    def __unicode__(self):
-        return self.teacher.user.username, self.hub.hubname
+        return self.name
 
 '''  end of user models '''
 
 ''' start of material models '''
 
+# model for super resource table
 class Resource(models.Model):
-    resourcename = models.CharField(max_length=128)
+    name = models.CharField(max_length=128)
+    
+    # comma separated field for easier tree searching
+    tree = models.TextField(null=True)
+  
+    # creates foreign key to teacher
+    author = models.ForeignKey(Teacher, null=True)#todo {properly implement foreign key}
+    
+    level = models.IntegerField()
     description = models.TextField()
     
-    # //todo comma separated tree for viewing evolution
-    #trees = models.CharField(max_length=128)
-    
-	# //todo foreignkeys
-    #author = models.ForeignKey(Teacher)
-    
     def __unicode__(self):
-        return self.resourcename
+        return self.name
         
-        
+# model for subclass of resource for files        
 class FilesResource(models.Model):
-    # one to one relationship
+    # one to one relationship with abstract resource
     resource = models.OneToOneField(Resource)
-
+    # path to resource
+    # todo {add a proper file manager}
     path = models.CharField(max_length=128)
 
     def __unicode__(self):
-        return self.resource.resourcename
-        
-class WebResource(models.Model):
-    # one to one relationship
-    resource = models.OneToOneField(Resource)
+        return self.resource.name
 
+# model for subclass of resource for web pages   
+class WebResource(models.Model):
+    # one to one relationship with abstract resource
+    resource = models.OneToOneField(Resource)
+    # url of web resource
     url = models.URLField()
 
     def __unicode__(self):
-        return self.resource.resourcename
-    
-class Tag(models.Model):
-	# //todo implement tags
-    #tagname = models.CharField(max_length=128)
-    
-    def __unicode__(self):
-        return self.tagname
-    
-class ResourceTag(models.Model):
-	# //todo foreignkeys
-    #resource = models.ForeignKey(Resource)
-    #tag = models.ForeignKey(Tag)
+        return self.resource.name
+        
+'''
+ADD NEW RESOURCES HERE
+class TemplateResource(models.Model):
+    # one to one relationship with abstract resource
+    resource = models.OneToOneField(Resource)
+
+    # your resource properties go here 
 
     def __unicode__(self):
-        return self.resource.resourcname, self.tag.tagname   
+        return self.resource.name
+''' 
+
+# model for a pack of resources
+class Pack(models.Model):
+    name = models.CharField(max_length=128)
+    # creates foreign key to teacher
+    author = models.ForeignKey(Teacher)
+    # many to many relationship with resource
+    resources = models.ForeignKey(Resource)
+
+# model for tags of resources and packs    
+class Tag(models.Model):
+    name = models.CharField(max_length=128)
     
+    def __unicode__(self):
+        return self.name  
+
+# intermediate model for the many to many relationship
+# between resources and tags        
+class ResourceTag(models.Model):
+    tags = models.ForeignKey(Tag)
+    resources = models.ForeignKey(Resource)
+    
+    def __unicode__(self):
+        return self.tags, self.resources
+
+# intermediate model for the many to many relationship
+# between resources and tags          
+class PackTag(models.Model):
+    tags = models.ForeignKey(Tag)
+    packs = models.ForeignKey(Pack)
+    
+    def __unicode__(self):
+        return self.tags.name, self.pack.tag
+        
 ''' end of material models '''
 
 ''' start of material relationship models '''
 
-# //todo enter teacher to resource reltionships here
+# model to store whether users downloaded a resource        
+class TeacherDownloadsResource(models.Model):
+    teacher = models.ForeignKey(Teacher)
+    resource = models.ForeignKey(Resource)
+    # used or not
+    used = models.BooleanField()
+    
+    def __unicode__(self):
+        return self.teacher.name, self.resource.name, self.used
+        
+# model to store where people used a resource
+class TeacherUsesResource(models.Model):
+    teacher = models.ForeignKey(Teacher)
+    resource = models.ForeignKey(Resource)
+    download = models.ForeignKey(TeacherDownloadsResource)
+    # lat and long of where it was used
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    # rated
+    rated = models.BooleanField()
+    
+    def __unicode__(self):
+        return self.teacher.name, self.resource.name, self.download.id
+        
+# model to store a users rating of a resource        
+class TeacherRatesResource(models.Model):
+    teacher = models.ForeignKey(Teacher)
+    teacher = models.ForeignKey(Teacher)
+    resource = models.ForeignKey(Resource)
+    # rating measures
+    measure1 = models.CharField(max_length=128)
+    measure2 = models.CharField(max_length=128)
+    measure3 = models.CharField(max_length=128)
+    # comment box
+    comment = models.TextField()
+    
+    def __unicode__(self):
+        return self.teacher.name, self.resource.name
+        
+# model to indicate if users want to talk about a resource        
+class TeacherWantstoTalkResource(models.Model):
+    teacher = models.ForeignKey(Teacher)
+    resource = models.ForeignKey(Resource)
+    # comment box
+    comment = models.TextField()
+    
+    def __unicode__(self):
+        return self.teacher.name, self.resource.name  
+     
+''' end of material relationship models '''    
 
-''' end of material realtionship models '''     
+''' start of discussion relationship models '''
+
+# model to disucssions of a resource        
+class TeacherRatesResource(models.Model):
+    owner = models.ForeignKey(Teacher)
+    resource = models.ForeignKey(Resource)
+    # many to many field for all teachers who participate in this discussion
+    attendees = models.ManyToManyField(Teacher)
+    # foreign key if discussion was at a hub (can be null if not at a hub)
+    hub = models.ForeignKey(Hub, null=True)
+    # if discussion was offline or online
+    physical = models.BooleanField()
+    # location of discussion
+    location = models.CharField(max_length=128)
+    # time of discussion
+    time = models.DateTimeField()
+    
+    def __unicode__(self):
+        return self.teacher.name, self.resource.name
+
+''' end of discussion relationship models '''
