e8be8b91120c81325f4327a571fd66ebd427bf52
hoodscott <scottdothood@gmail.com>
2015-12-01 11:15:31 +0000

    Implementing a basic tag view/explore page #23
    New pages show all resources with a given tag.  The resource page tags
    link back to these.

--- django_project/treasure/urls.py
+++ django_project/treasure/urls.py
@@ -26,5 +26,8 @@ urlpatterns = patterns('',
         url(r'^hub/(?P<hub_id>\w+)/$', views.hub_view, name='hub_view'),
         url(r'^school/(?P<school_id>\w+)/$', views.school_view, name='school_view'),
         
+        url(r'^explore/$', views.explore, name='explore'),
+        url(r'^explore/(?P<tag_id>\w+)/$', views.tag, name='tag'),
+                
         url(r'^search/$', views.search, name='search'),
         )
