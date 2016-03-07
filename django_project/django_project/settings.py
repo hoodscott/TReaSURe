d4171d413312093b55e78acb0c7827a96f4298e6
Dimitrios <2014573k@student.gla.ac.uk>
2016-03-07 17:34:32 +0000

    again, and again and again

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -94,7 +94,7 @@ MEDIA_ROOT = MEDIA_PATH
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = 'plancsharing/secret/'
+MEDIA_URL = '/secret/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
