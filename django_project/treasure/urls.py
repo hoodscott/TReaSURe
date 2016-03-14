45086acedbe6fe29fcda95152a55ee098a1befe0
hoodscott <scottdothood@gmail.com>
2016-03-14 02:33:14 +0000

    Implementing django registration
    Need to redefine user registration as now users can be created without having a teacher object attached.

--- django_project/treasure/urls.py
+++ django_project/treasure/urls.py
@@ -10,7 +10,6 @@ urlpatterns = patterns('',
         url(r'^me/$', views.index, name='my_homepage'),
         url(r'^help/$', views.help, name='help'),
         
-        url(r'^register/$', views.register, name='register'),
         url(r'^login/$', views.user_login, name='login'),
         url(r'^logout/$', views.user_logout, name='logout'),
         url(r'^password_reset/$', 'django.contrib.auth.views.password_reset',
